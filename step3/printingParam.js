d7b116c60565eb3d6b35fe92d2396563a1b1e901
Bassim <bassimsadik@hotmail.com>
2017-03-13 21:37:51 +0000

    made it a module and corrected path files on previous documents

--- step3/printingParam.js
+++ step3/printingParam.js
@@ -17,7 +17,7 @@ function getAndPrintHTML (options) {
 }
   var requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
+    path: '/http-examples/step3.html'
   };
 getAndPrintHTML(requestOptions);
 
--- step5/http-functions.js
+++ step5/http-functions.js
@@ -1,8 +1,7 @@
 var https = require('https');
 
-function getHTML(options, callback) {
-
-var buffer = '';
+module.exports = function getHTML (options, callback) {
+    var buffer = '';
 
   https.get(options, function(response) {
   response.setEncoding('utf8');
@@ -14,17 +13,4 @@ var buffer = '';
     callback(buffer);
   });
   });
-}
-
-
-function printHTML (html) {
-  console.log(html);
-}
-
-var requestOptions = {
-  host: 'sytantris.github.io',
-  path: '/http-examples/step4.html'
 };
-
-getHTML(requestOptions, printHTML);
-
