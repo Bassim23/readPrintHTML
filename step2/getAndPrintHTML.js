e58b6147e6dcf81d6b144db6ea56933d84c25114
Bassim <bassimsadik@hotmail.com>
2017-03-13 20:04:59 +0000

    deleted comments

--- step2/getAndPrintHTML.js
+++ step2/getAndPrintHTML.js
@@ -24,35 +24,3 @@ function getAndPrintHTMLChunks () {
 });
 }
 getAndPrintHTMLChunks();
-
-
-
-
-// var https = require('https');
-// var fs = require('fs');
-
-// function getAndPrintHTML() {
-
-//   var requestOptions = {
-//     host: 'sytantris.github.io',
-//     path: '/http-examples/step1.html'
-//   };
-
-// // https.get(requestOptions, function(response) {
-//   var myReadStream = fs.createReadStream(requestOptions.host.path);
-//   myReadStream.setEncoding('UTF8');
-//   myReadStream.on('data', function(chunk) {
-//     console.log('new chunk received');
-//     console.log(chunk);
-//   })
-// //   response.setEncoding('utf8');
-// //   response.on('data', function (data){
-// //     console.log("Chunk received. Length: ", data.length);
-// //     console.log(data.toString()+'\n');
-// //   });
-// //   response.on('end', function() {
-// //     console.log("Response stream complete.");
-// //   });
-// // });
-// }
-// getAndPrintHTML();
